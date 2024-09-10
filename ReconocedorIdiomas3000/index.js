98fe778997d0a11ef08044fa682d16f11b379690
Charly <carlosecimino@gmail.com>
2024-09-10 09:30:43 -0300

    arreglado?

--- ReconocedorIdiomas3000/index.js
+++ ReconocedorIdiomas3000/index.js
@@ -2,7 +2,9 @@
 const {reconocerIdioma} = require('./reconocedor'); //Desestructuración del objeto
 
 //Leer archivo de texto y normalizar la cadena
-const texto = "Este programa se tiene que arreglar porque da que todo esta escrito en ingles y no puede ser";
+//const texto = "Este programa se tiene que arreglar porque da que todo esta escrito en ingles y no puede ser";
+const texto = "was a swing band leader and leading figure in black British music of the 1930s and 1940s. Born in British Guiana, he was educated in Britain and travelled to New York to immerse himself in the Harlem jazz scene. He returned to Britain and established the Aristocrats (or Emperors) of Jazz, a mainly black swing band, with Leslie Thompson. In 1937 Johnson took control of the band through a legal loophole, causing the departure of Thompson and several musicians. Johnson filled the vacancies with Caribbean musicians, the band's popularity grew, and it changed its name to the West Indian Dance Orchestra. In 1938 the band broadcast on BBC Radio, recorded their first discs and appeared in an early television broadcast. Johnson was considered a pioneer for black musical leaders in the UK.";
+//const texto = "LALA";
 
 let textoArreglado = texto.toLowerCase().replace(/[^a-z]/g, ""); //Replace --> expresión regular
 
--- ReconocedorIdiomas3000/reconocedor.js
+++ ReconocedorIdiomas3000/reconocedor.js
@@ -23,52 +23,55 @@ const crearVecOcurrencias = (texto) => {
 };
 
 //Calcular el ECM
-ES = [
-  11.05, 0.87, 6.82, 4.77, 11.93, 1.18, 0.93, 0.37, 8.91, 0.24, 0.01, 5.65, 2.7,
-  7.41, 9.51, 2.29, 0.78, 5.46, 8.14, 5.09, 4.21, 0.76, 0.01, 0.11, 0.7, 0.1,
-];
-EN = [
-  7.81, 1.28, 2.93, 4.11, 13.05, 2.88, 1.39, 5.85, 6.77, 0.23, 0.42, 3.6, 2.62,
-  7.28, 8.21, 2.15, 0.14, 6.64, 6.46, 9.02, 2.77, 1, 1.49, 0.3, 1.51, 0.09,
-];
-DE = [
-  6.11, 1.87, 2.67, 5.56, 16.51, 1.48, 2.89, 4.15, 8.26, 0.26, 1.67, 3.4, 2.72,
-  10.28, 3.03, 1.01, 0.02, 7.83, 6.27, 6.09, 4.48, 1.08, 1.2, 0.05, 0.08, 1.03,
-];
+dataIdiomas = {
+  ES: [
+    11.05, 0.87, 6.82, 4.77, 11.93, 1.18, 0.93, 0.37, 8.91, 0.24, 0.01, 5.65, 2.7,
+    7.41, 9.51, 2.29, 0.78, 5.46, 8.14, 5.09, 4.21, 0.76, 0.01, 0.11, 0.7, 0.1,
+  ],
+  EN: [
+    7.81, 1.28, 2.93, 4.11, 13.05, 2.88, 1.39, 5.85, 6.77, 0.23, 0.42, 3.6, 2.62,
+    7.28, 8.21, 2.15, 0.14, 6.64, 6.46, 9.02, 2.77, 1, 1.49, 0.3, 1.51, 0.09,
+  ],
+  DE: [
+    6.11, 1.87, 2.67, 5.56, 16.51, 1.48, 2.89, 4.15, 8.26, 0.26, 1.67, 3.4, 2.72,
+    10.28, 3.03, 1.01, 0.02, 7.83, 6.27, 6.09, 4.48, 1.08, 1.2, 0.05, 0.08, 1.03,
+  ]
+}
+
+const suma = (a, b) => a + b;
 
 const codigoIdiomaConMenorError = (vecOcurrencias) => {
-  const sumatoria = vecOcurrencias.reduce((a, b) => a + b);
-
+  const sumatoria = vecOcurrencias.reduce(suma);
   //vecOcurrencias = vecOcurrencias.map((numAbsoluto, i) => Math.pow(ES[i] - (numAbsoluto /sumatoria), 2));
-  let vecOcurrenciasES = vecOcurrencias.map((numAbsoluto, i) =>
-    Math.pow(ES[i] - (numAbsoluto / sumatoria), 2)
-  );
-  const ecmES =
-    //vecOcurrenciasES.reduce((a, b) => a + b) / vecOcurrenciasES.length;
-    (vecOcurrenciasES.reduce((a, b) => (a + b)))/ vecOcurrenciasES.length;
 
-  let vecOcurrenciasEN = vecOcurrencias.map((numAbsoluto, i) =>
-    Math.pow(EN[i] - (numAbsoluto / sumatoria), 2)
-  );
-  const ecmEN =
-    //vecOcurrenciasEN.reduce((a, b) => a + b) / vecOcurrenciasEN.length;
-    (vecOcurrenciasEN.reduce((a, b) => (a + b)))/ vecOcurrenciasEN.length;
+  const clavesIdiomas = Object.keys(dataIdiomas);
 
-  let vecOcurrenciasDE = vecOcurrencias.map((numAbsoluto, i) =>
-    Math.pow(DE[i] - (numAbsoluto / sumatoria), 2)
-  );
-  const ecmDE =
-    //vecOcurrenciasDE.reduce((a, b) => a + b) / vecOcurrenciasDE.length;
-    (vecOcurrenciasDE.reduce((a, b) => (a + b)))/ vecOcurrenciasDE.length;
+  const losEcms = clavesIdiomas.map( claveIdioma => {
+    const data = dataIdiomas[claveIdioma];
+    return vecOcurrencias.map(numAbsoluto => numAbsoluto / sumatoria)
+    .map((valRel, i) => {
+      console.log(data[i] , valRel * 100)
+        return Math.pow(data[i] - valRel * 100, 2) 
+      })
+    .reduce(suma) / vecOcurrencias.length;
+    //.map( (x, y, v) => {console.table(v); return x})
+  });
 
+  console.table(losEcms);
+  return losEcms.reduce( (a, b) => a < b ? a : b);
+  
+
+  /*
+console.log(vecOcurrenciasES)
+  
   console.log(ecmES);
   console.log(ecmEN);
   console.log(ecmDE);
 
   const cod =
     ecmES < ecmEN && ecmES < ecmDE ? "ES" : ecmEN < ecmDE ? "EN" : "DE";
-
-  return cod;
+*/
+  //return cod;
 };
 
 module.exports = {
